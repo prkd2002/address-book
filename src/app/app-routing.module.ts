70a3a3ee1047bae670b0147c31c982c95bc3716c
prkd2002 <prkd2002@gmail.com>
2024-08-13 08:54:54 +0200

    add nav-bar and sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {NewcontactComponent} from "./newcontact/newcontact.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+},
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+  {
+    path: 'newcontact',
+    component: NewcontactComponent,
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+//Angular Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NewcontactComponent } from './newcontact/newcontact.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    NewcontactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [
     provideAnimationsAsync()
